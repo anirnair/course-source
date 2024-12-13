5c3aa78fcac10dfa7bb896ae660764a126050359
anirnair <anirudh.nair@juspay.in>
2024-12-13 17:21:54 +0530

    tset commi

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Move Around & <br/> find out!</h1>
-    <p>This is Falala Bilala.</p>
+    <p>another change is being done.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
